2bbc0cc74c66e16658cca2a182fd8da774552fac
Tanya Mehta <tanyamehta@berkeley.edu>
2022-11-04 23:42:23 -0700

    finished hw5

--- react-app/src/Components/Feed.js
+++ react-app/src/Components/Feed.js
@@ -1,37 +1,42 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import axios from 'axios';
 import Post from "./Post";
 import NewPost from "./NewPost";
 
 const Feed = () => {
-  // Un-comment the lines below to complete your solution
-  // ====================
+    // Un-comment the lines below to complete your solution
+    // ====================
 
-  // const [_________, _________] = useState();
+    const [data, setData] = useState();
 
-  // const getPostsData = () => {
-  //   axios
-  //     .get(_________) //THIS IS YOUR URL OF YOUR API
-  //     .then(__________) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
-  //     .catch(__________);  //ERROR CATCHING IN CASE WE RECIEVE AN ERROR
-  // };
+    const getPostsData = () => {
+        axios
+            .get("http://localhost:3002/posts") //THIS IS YOUR URL OF YOUR API
+            .then((data) => setData(data.data)) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
+            .catch((error) => console.log(error));  //ERROR CATCHING IN CASE WE RECIEVE AN ERROR
+    };
 
-  // useEffect(() => {
-  //   _________();
-  // }, [])
+    useEffect(() => {
+        getPostsData();
+    }, [])
 
-  // ====================
+    // ====================
 
-  return (
-    <div style={{ maxWidth: '600px', marginLeft: 'auto', marginRight: 'auto'}}>
-      {
-        data && data.map(d =>
-          <Post title={d.title} body={d.body} comments={d.comments} id={d.id} key={d.id} />
-        )
-      }
+    return (
+        <div style={{ maxWidth: '600px', marginLeft: 'auto', marginRight: 'auto' }}>
+            {
+                data && data.map(d =>
+                    <Post title={d.title} body={d.body} comments={d.comments} id={d.id} key={d.id} />
+                )
+            }
 
-      <NewPost __________ />
-    </div>
-  )
+            <NewPost display={(in_data) => {
+                let tempList = [...data];
+                tempList.push(in_data);
+                setData(tempList);
+            }} />
+        </div>
+    )
 
 }
 
--- react-app/src/Components/NewPost.js
+++ react-app/src/Components/NewPost.js
@@ -1,35 +1,39 @@
-import {_________} from "react";
+
+
+import { React, useState } from "react";
 import axios from "axios";
 
-const NewPost = ({ _________ }) => {
-  const [id, setId] = useState();
-  const [title, setTitle] = useState();
-  const [body, setBody] = useState();
 
-  const onSubmit = () => {
-    console.log({
-      id,
-      title,
-      body
-    })
+const NewPost = (props) => {
+    const [id, setId] = useState("");
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
 
-    axios.post(_________, _________).then(_________);
-  }
+    const onSubmit = () => {
+        console.log({
+            id,
+            title,
+            body
+        })
 
-  return <div>
-    <div>
-      <input type="text" placeholder="ID" value={id} onChange={e => setId(e.target.value)} />
+        // TODO: Posted result is not GOTTEN
+        axios.post("http://localhost:3002/post", { id: id, title: title, body: body }).then((data) => props.display(data.data));
+    }
+
+    return <div>
+        <div>
+            <input type="text" placeholder="ID" value={id} onChange={e => setId(e.target.value)} />
+        </div>
+        <div>
+            <input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
+        </div>
+        <div>
+            <input type="text" placeholder="Body" value={body} onChange={e => setBody(e.target.value)} />
+        </div>
+        <button style={{ marginTop: '4px' }} onClick={onSubmit}>
+            Submit
+        </button>
     </div>
-    <div>
-      <input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
-    </div>
-    <div>
-      <input type="text" placeholder="Body" value={body} onChange={e => setBody(e.target.value)} />
-    </div>
-    <button style={{ marginTop: '4px'}} onClick={onSubmit}>
-      Submit
-    </button>
-  </div>
 }
 
 export default NewPost;
